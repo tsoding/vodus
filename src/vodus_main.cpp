5649fa76b4b11be3a7a3944977f135b01d9a4e0e
rexim <reximkut@gmail.com>
2020-03-19 02:37:29 +0700

    Sort message in chronological order

--- src/vodus_main.cpp
+++ src/vodus_main.cpp
@@ -371,6 +371,8 @@ int main(int argc, char *argv[])
             encode_avframe(context, frame, packet, output_stream);
         };
 
+    // TODO: log is not retrived directly from the Twitch API
+    //   See https://github.com/PetterKraabol/Twitch-Chat-Downloader
     String_View input = file_as_string_view(log_filepath);
     while (input.count > 0) {
         assert(messages_size < VODUS_MESSAGES_CAPACITY);
@@ -380,8 +382,11 @@ int main(int argc, char *argv[])
         messages[messages_size].message = message.trim();
         messages_size++;
     }
-
     messages_size = std::min(messages_size, 20lu);
+    std::sort(messages, messages + messages_size,
+              [](const Message &m1, const Message &m2) {
+                  return m1.timestamp < m2.timestamp;
+              });
 
     sample_chat_log_animation(face, encode_frame, &bttv);
 
