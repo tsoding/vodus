746cb6248ccb3edd44e8e4cd03cab1641c4f5d32
rexim <reximkut@gmail.com>
2020-03-26 02:11:34 +0700

    Add TODO(#45)

--- src/vodus_main.cpp
+++ src/vodus_main.cpp
@@ -356,7 +356,7 @@ int main(int argc, char *argv[])
     }
 
     // TODO(#44): BTTV mapping is not auto populated from the BTTV API
-    // TODO: FFZ mapping is not auto populated from the FFZ API
+    // TODO(#45): FFZ mapping is not auto populated from the FFZ API
     Emote_Cache emote_cache = { };
     auto mapping_csv = file_as_string_view("./mapping.csv");
     while (mapping_csv.count > 0) {
