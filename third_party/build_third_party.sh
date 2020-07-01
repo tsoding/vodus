4d2930e08c6dd5aafa01587c67bec288e8b3c528
rexim <reximkut@gmail.com>
2020-07-02 03:24:00 +0700

    (#4) Cache ffmpeg build

--- third_party/build_third_party.sh
+++ third_party/build_third_party.sh
@@ -2,13 +2,15 @@
 
 FFMPEG_VERSION=4.3
 
-wget "https://ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.xz" 2>&1 > /dev/null
-tar fvx "ffmpeg-${FFMPEG_VERSION}.tar.xz" 2>&1 > /dev/null
-mkdir "ffmpeg-${FFMPEG_VERSION}-dist"
-
-cd "ffmpeg-${FFMPEG_VERSION}"
-./configure --disable-doc --disable-programs
-make -j5
-DESTDIR="../ffmpeg-${FFMPEG_VERSION}-dist" make install
+if [ ! -d "ffmpeg-${FFMPEG_VERSION}-dist" ]; then
+    wget "https://ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.xz" 2>&1 > /dev/null
+    tar fvx "ffmpeg-${FFMPEG_VERSION}.tar.xz" 2>&1 > /dev/null
+    mkdir "ffmpeg-${FFMPEG_VERSION}-dist"
+    
+    cd "ffmpeg-${FFMPEG_VERSION}"
+    ./configure --disable-doc --disable-programs
+    make -j5
+    DESTDIR="../ffmpeg-${FFMPEG_VERSION}-dist" make install
+fi
 
 
