9da1925c98d8c53f4bc33287081582062ca2257c
rexim <reximkut@gmail.com>
2020-07-02 02:57:49 +0700

    (#4) install nasm for third party dependency build

--- third_party/build_third_party.sh
+++ third_party/build_third_party.sh
@@ -2,8 +2,8 @@
 
 FFMPEG_VERSION=4.3
 
-wget "https://ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.xz"
-tar fvx "ffmpeg-${FFMPEG_VERSION}.tar.xz"
+wget "https://ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.xz" 2>&1 > /dev/null
+tar fvx "ffmpeg-${FFMPEG_VERSION}.tar.xz" 2>&1 > /dev/null
 cd "ffmpeg-${FFMPEG_VERSION}"
 ./configure
 make -j5
